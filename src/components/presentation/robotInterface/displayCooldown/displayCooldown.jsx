0686abf0a6b86f0cb9b612d46de6eab631823966
Jillian Ogle <jill@letsrobot.tv>
2020-04-11 13:03:42 -0700

    use monospace font for cooldown, format count so button size doesn't change.

--- src/components/presentation/robotInterface/displayCooldown/displayCooldown.jsx
+++ src/components/presentation/robotInterface/displayCooldown/displayCooldown.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./displayCooldown.scss";
 
 const DisplayCooldown = ({ count, cooldown }) => {
+  const [displayCount, setDisplayCount] = useState(count || cooldown);
+
+  useEffect(() => {
+    if (count && cooldown) handleDigits();
+  });
+
+  const handleDigits = () => {
+    const dif = getlength(cooldown) - getlength(count);
+    let formatCount = count;
+    for (let i = 0; i < dif; i++) formatCount = " " + formatCount;
+    setDisplayCount(formatCount);
+    console.log("Display Count: ", displayCount);
+  };
+
+  const getlength = number => {
+    return number.toString().length;
+  };
+
   return (
     <span className="cooldown__container">
-      <span className="cooldown__time">{count || cooldown}</span>
-      {` /`}
-      <span className="cooldown__time">{cooldown}</span>
+      <span className="cooldown__time-remainder">{displayCount}</span>
+      <span className="cooldown__time-divider">{`/`}</span>
+      <span className="cooldown__time-total">{cooldown}</span>
     </span>
   );
 };

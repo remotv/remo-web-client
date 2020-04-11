444ef738116a388ec6fe3350606cebdecb9f5e44
Jillian Ogle <jill@letsrobot.tv>
2020-04-11 13:37:22 -0700

    remove console logs

--- src/components/presentation/robotInterface/displayCooldown/displayCooldown.jsx
+++ src/components/presentation/robotInterface/displayCooldown/displayCooldown.jsx
@@ -11,12 +11,7 @@ const DisplayCooldown = ({ count, cooldown }) => {
   const handleDigits = () => {
     const dif = getlength(cooldown) - getlength(count);
     let formatCount = count;
-    console.log("Dif: ", dif);
-    for (let i = 0; i < dif; i++) {
-      formatCount = " " + formatCount;
-      console.log("Loop: ", i);
-    }
-    console.log("Format Count////", formatCount, "/////");
+    for (let i = 0; i < dif; i++) formatCount = " " + formatCount;
     setDisplayCount(formatCount);
   };
 

f1a269138db9ae5b8807591cb0801a80c89b9c67
Jillian Ogle <jill@letsrobot.tv>
2020-05-12 12:50:14 -0700

    templates readme

--- src/components/templates/inlineItem/index.js
+++
@@ -1,6 +0,0 @@
-//attempting to templetize inline items
-
-import InlineResponseHandler from "../../functional/inlineResponseHandler";
-import InlineInput from "../../common/inlineInput";
-
-export { InlineResponseHandler, InlineInput };

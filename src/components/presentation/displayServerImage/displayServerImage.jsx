6bd75560381f66694a9bda6c20e99eb8eb12315c
Jillian Ogle <jill@letsrobot.tv>
2020-06-05 15:40:58 -0700

    handle image loading error

--- src/components/presentation/displayServerImage/displayServerImage.jsx
+++ src/components/presentation/displayServerImage/displayServerImage.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import getServerImage from "../../functional/getServerImage/index";
 import "./displayServerImage.scss";
 
 const DisplayServerImage = ({ otherImage, ...props }) => {
-  const image = otherImage || getServerImage({ ...props });
-  return <img className="displayServerImage__display" src={image} alt="" />;
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    setImage(otherImage || getServerImage({ ...props }));
+  });
+
+  const handleError = () => setImage(getServerImage(null));
+  return (
+    <img
+      className="displayServerImage__display"
+      src={image}
+      alt=""
+      onError={() => handleError()}
+    />
+  );
 };
 
 export default DisplayServerImage;
